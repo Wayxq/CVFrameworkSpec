ad4710234a594438211b6e5eab9ef6952b7c1dbd
Aoyun <Aoyun>
2022-02-23 21:07:52 +0800

    add framework

---
+++ CareVoice.framework/Headers/CareVoice.h
@@ -0,0 +1,18 @@
+//
+//  CareVoice.h
+//  CareVoice
+//
+//  Created by aoyun on 2022/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CareVoice.
+FOUNDATION_EXPORT double CareVoiceVersionNumber;
+
+//! Project version string for CareVoice.
+FOUNDATION_EXPORT const unsigned char CareVoiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CareVoice/PublicHeader.h>
+
+
